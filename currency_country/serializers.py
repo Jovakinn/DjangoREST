e0d84d3dd05c2e42d8f26f53a95c29e78500520b
Jovakinn <max05012004@gmail.com>
2022-02-12 22:52:19 +0200

    Initial commit

---
+++ currency_country/serializers.py
@@ -0,0 +1,26 @@
+from rest_framework import serializers, generics
+from .models import Country, Snippet, User
+
+
+class CountrySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Country  # this is the model that is being serialized
+        fields = ('country_name', 'local_currency')
+
+
+class SnippetSerializer(serializers.HyperlinkedModelSerializer):
+    owner = serializers.ReadOnlyField(source='owner.username')
+    highlight = serializers.HyperlinkedIdentityField(view_name='snippet-highlight', format='html')
+
+    class Meta:
+        model = Snippet
+        fields = ['url', 'id', 'highlight', 'owner',
+                  'title', 'code', 'linenos', 'language', 'style']
+
+
+class UserSerializer(serializers.HyperlinkedModelSerializer):
+    snippets = serializers.HyperlinkedRelatedField(many=True, view_name='snippet-detail', read_only=True)
+
+    class Meta:
+        model = User
+        fields = ['url', 'id', 'username', 'snippets']
---
+++ currency_country/views.py
@@ -0,0 +1,32 @@
+from rest_framework import permissions, renderers, viewsets
+from rest_framework.decorators import action
+from rest_framework.response import Response
+
+from .models import Country, User, Snippet
+from .permissions import IsOwnerOrReadOnly
+from .serializers import CountrySerializer, UserSerializer, SnippetSerializer
+
+
+class CountryList(viewsets.ReadOnlyModelViewSet):
+    queryset = Country.objects.all()
+    serializer_class = CountrySerializer
+
+
+class SnippetViewSet(viewsets.ModelViewSet):
+    queryset = Snippet.objects.all()
+    serializer_class = SnippetSerializer
+    permission_classes = [permissions.IsAuthenticatedOrReadOnly,
+                          IsOwnerOrReadOnly]
+
+    @action(detail=True, renderer_classes=[renderers.StaticHTMLRenderer])
+    def highlight(self, request, *args, **kwargs):
+        snippet = self.get_object()
+        return Response(snippet.highlighted)
+
+    def perform_create(self, serializer):
+        serializer.save(owner=self.request.user)
+
+
+class UserViewSet(viewsets.ReadOnlyModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
